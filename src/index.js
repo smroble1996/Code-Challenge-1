fb1105d33f8c3f1e238e6029adea3e41ff8c6707
Sam Roble <smroble1996@gmail.com>
2021-07-29 11:55:56 -0600

    submission for deadline

--- src/index.js
+++ src/index.js
@@ -1,1 +1,25 @@
 // write your code here
+// write your code here
+const fgImage = document.getElementById('fg-image');
+
+fetch('http://localhost:3000')
+    // 'Content-Type' : 'db.json',
+    // 'Accept' : 'db.json'
+    .then((res) => res.json())
+    .then(handleData);
+
+    function handleData(dogs){
+        dogs.forEach(images => {
+            console.log(images.id)
+        const fgSpan = document.createElement('span');
+            fgSpan.innerText = images.id
+            fgImage.append(fgSpan);
+            
+
+        });
+    }
+    function addComents(){
+        dogs.forEach(comments => {
+            console.log(comments.id)
+        });
+    }
